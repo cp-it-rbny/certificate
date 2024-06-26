50f68c15010f9b608aa8d310a6c9c5bf9ac67a72
cp-it-rbny <hermano.crubellati@newyorkredbulls.com>
2024-06-26 15:25:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,72 @@
-const http = require('http');
-const fs = require('fs');
-const {jsPDF} = require('jspdf');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
+const { jsPDF } = require('jspdf');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile('index.html', function (err, data) {
-            if (err) {
-                res.statusCode = 500;
-                res.end(`Error getting the file: ${err}.`);
-            } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
-        });
-    } else {
-        if (req.url === '/certificate') {
-            // Default export is a4 paper, portrait, using millimeters for units
-            const doc = new jsPDF('landscape');
-            doc.text("Hello world!", 10, 10);
-            doc.save("a4.pdf"); // will save the file in the current working directory
-            doc.output('dataurlnewwindow',"certificate.pdf")
-            res.statusCode = 200;
-            res.end("ok");
-        } else {
-            res.statusCode = 404;
-            res.end('Not Found\n');
-        }
-    }
-    });
-
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Server running at http://127.0.0.1:3000/');
+app.get("/", (req, res) => res.type('html').send(html));
+app.get("/certificate", (req, res) =>{
+    const doc = new jsPDF('landscape');
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf"); // will save the file in the current working directory
+    doc.output('dataurlnewwindow', "certificate.pdf")
+    res.statusCode = 200;
+    res.end("ok");
 });
+   
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
